c32aa9be1a169cabb84ebbc7145affed715c6a33
root <root@vignesh-pt3293.csez.zohocorpin.com>
2020-04-04 08:27:56 +0530

    Updates

---
+++ precache-manifest.67f6beffb860158d13b98ef122607f9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5898505d9e63d60abccbc5bf836b7ff",
+    "url": "/secreat/index.html"
+  },
+  {
+    "revision": "cdbc19d991f12d326ab6",
+    "url": "/secreat/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5411febaf6b12e9c9b99",
+    "url": "/secreat/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "cdbc19d991f12d326ab6",
+    "url": "/secreat/static/js/2.f896c0ce.chunk.js"
+  },
+  {
+    "revision": "6e91b8fde0e4d24ffac100669f6dd64c",
+    "url": "/secreat/static/js/2.f896c0ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5411febaf6b12e9c9b99",
+    "url": "/secreat/static/js/main.b4d100c6.chunk.js"
+  },
+  {
+    "revision": "3ae722012c9614814711",
+    "url": "/secreat/static/js/runtime-main.f88b664f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/secreat/static/media/logo.5d5d9eef.svg"
+  }
+]);
