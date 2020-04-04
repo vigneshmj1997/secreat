f961a68b0f7243ea772ac2b8762fd8ad99acbbd6
root <root@vignesh-pt3293.csez.zohocorpin.com>
2020-04-04 22:31:06 +0530

    Updates

--- precache-manifest.6d683d44c74773e85dc6550e856e584f.js
+++ precache-manifest.6d683d44c74773e85dc6550e856e584f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8be7b6225cbb29ad1e6b148c25a548a",
+    "revision": "5d2d507f7dfbf76b9a67029df9a3b8e9",
     "url": "/secreat/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/secreat/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "e5afc9fa8e6c5b592cd8",
+    "revision": "cad920503d8de71b7920",
     "url": "/secreat/static/css/main.050523cc.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/secreat/static/js/2.6a5ebf74.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e5afc9fa8e6c5b592cd8",
-    "url": "/secreat/static/js/main.82553363.chunk.js"
+    "revision": "cad920503d8de71b7920",
+    "url": "/secreat/static/js/main.13ababb0.chunk.js"
   },
   {
     "revision": "3ae722012c9614814711",
